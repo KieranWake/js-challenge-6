0ffe92fa4993e4b88e97132d19a5c3b356e29c4d
Kieran Wake <kieranwake1234@gmail.com>
2024-02-07 19:19:44 +0000

    printed to console

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 function largestSwap(num) {
     let numStr = num.split("");
+    console.log(numStr);
+    
 
-    if (digit1 > digit2) {
-        return true;
-    } else if (digit2 > digit1) {
-        return false;
-    } else {
-        return true; 
-    }
 }
 
-console.log(largestSwap(72));
 
 //Instead of splitting the array using two seperate variables, let's use the reverse and 
 // join methods, then conclude with a comparison. 
