a99b88b2c1d4226a98604ea0c5223dfcb764e603
Kieran Wake <kieranwake1234@gmail.com>
2024-02-07 18:28:30 +0000

    implemented conditional statement

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ function largestSwap(num) {
 
     const digit1 = parseInt(numStr[0]);
     const digit2 = parseInt(numStr[1]);
+
+    if (digit1 > digit2) {
+        return true;
+    } else if (digit2 > digit1) {
+        return true; 
+    }
 	
 }
