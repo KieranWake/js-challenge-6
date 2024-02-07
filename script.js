5cd06b6fb8c34d01e59e3b754ec0d75395fd4475
Kieran Wake <kieranwake1234@gmail.com>
2024-02-07 19:17:42 +0000

    split the current parameter into string

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function largestSwap(num) {
-    const numStr = num.toString(); 
-
-    const digit1 = parseInt(numStr[0]);
-    const digit2 = parseInt(numStr[1]);
+    let numStr = num.split("");
 
     if (digit1 > digit2) {
         return true;
